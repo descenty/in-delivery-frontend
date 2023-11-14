a675829661664bc483c5b96b99a01b1f700c6da9
Deverty <bychenkov.a.k@edu.mirea.ru>
2023-11-14 06:02:43 +0300

    change padding

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Catalog from "@/components/catalog/catalog";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between w-full gap-8 p-12 px-28">
+    <main className="flex min-h-screen flex-col justify-between w-full gap-10 p-10 px-28">
       <h1 className="text-2xl font-semibold">Каталог</h1>
       <Catalog />
     </main>
